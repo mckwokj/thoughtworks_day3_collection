097d0bcfbf925f5614368cc16b6f3d56a40293e6
JOHNSON KWOK (DEV-DIGITAL-ISD-OOCLL/HKG) <KWOKJO2@OOCL.COM>
2021-12-01 11:50:18 +0800

    [Collection] [Johnson Kwok] feat: Complete filterEven

---
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.collection;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilter {
+
+    public List<Integer> filterEven(List<Integer> numbers) {
+        return numbers
+                .stream()
+                .filter(number -> number % 2 == 0)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> filterEndWithS(List<String> words) {
+
+    }
+
+    public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
+        return null;
+    }
+
+
+}
